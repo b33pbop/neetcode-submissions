3af899f350380c2dc4e84bba3a48e6cef92c7d4c
Jingling <121413938+b33pbop@users.noreply.github.com>
2026-05-07 16:34:43 +0800

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # use 2 pointers at the start and end
+        # for each character, we check if isalnum then lower or upper it
+
+        i = 0
+        j = len(s) - 1
+
+        while i <= j:
+            front = s[i]
+            back = s[j]
+
+            if front.isalnum() and back.isalnum():
+                front = front.lower()
+                back = back.lower()
+
+                if front != back:
+                    return False
+
+                i += 1
+                j -= 1
+            else:
+                if not front.isalnum():
+                    i += 1
+                if not back.isalnum():
+                    j -= 1
+        
+        return True
+            
