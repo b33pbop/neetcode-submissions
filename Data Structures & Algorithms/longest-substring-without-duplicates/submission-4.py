8e9f1eb72a970c0c375b334d43b97b7b424b1e72
Jingling <121413938+b33pbop@users.noreply.github.com>
2026-05-09 17:30:18 +0800

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        "advdf" -> [a, d, v]
+        move start from a till d
+        """
+        # sliding window
+        # use a set to check for duplicates
+        substring_set = set()
+        start = 0
+        end = 0
+        longest = 0
+
+        while end < len(s):
+            # move start to the first instance of the repeated character
+            if s[end] in substring_set:
+                substring_set.discard(s[start])
+                start += 1
+            else:
+                substring_set.add(s[end])
+                longest = max(longest, end - start + 1)
+                end += 1
+            
+        return longest
